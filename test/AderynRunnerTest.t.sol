38d272eeaa2a9148f0aa3c22ae5f1b1a5368e3e9
TilakMaddy <tilak.madichetti@gmail.com>
2023-12-10 13:44:34 +0530

    test samples for checking against aderyn

--- test/AderynRunnerTest.t.sol
+++ test/AderynRunnerTest.t.sol
@@ -2,7 +2,8 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import {Test} from "forge-std/Test.sol";
-import {AderynRunnerTool} from "../src/AderynRunnerTool.sol";
+import "../src/AderynRunnerTool.sol";
+import {console} from "forge-std/console.sol";
 
 contract AderynRunnerTest is Test {
     
@@ -11,7 +12,20 @@ contract AderynRunnerTest is Test {
     }
 
     function testAderynRunnerCustomWorks() public {
-        AderynRunnerTool.run_with("../aderyn/target/release/aderyn", "my_report.md");
+        AderynRunnerTool.run_with("../aderyn/target/release/aderyn", "my_report.md", "./reports/my_report.json");
+    }
+
+    function testThereAreNoCriticalErrors() public view {
+        IssueCount memory ic = AderynRunnerTool.get_issue_count("./reports/my_report.json");
+        assert(ic.critical == 0);
+        assert(ic.high == 0);
+    }
+
+    function testThereAreLessThan10MediumOrLessSevereErrors() public view {
+        IssueCount memory ic = AderynRunnerTool.get_issue_count("./reports/my_report.json");
+        assert(ic.medium < 10);
+        assert(ic.low < 10);
+        assert(ic.nc < 10);
     }
 
 }
